40487c3de7cea169b3cb5d65421c34359bf3b818
Katelyn Luther <katelynluther87@aim.com>
2023-04-19 11:33:06 -0600

    complete

--- server/app.py
+++ server/app.py
@@ -21,8 +21,45 @@ def index():
 
 @app.route('/restaurants')
 def restaurants():
+    return make_response([restaurant.to_dict() for restaurant in Restaurant.query.all()], 200)
 
-    pass
+@app.route('/restaurants/<int:id>', methods=['GET', 'DELETE'])
+def restaurants_by_id(id):
+    restaurant = Restaurant.query.filter_by(id=id).first()
+
+    if not restaurant:
+        return make_response({'error': '404: Restaurant not found'}, 404)
+    elif request.method == 'GET':
+        return make_response(restaurant.to_dict(), 200)
+    elif request.method == 'DELETE':
+        db.session.delete(restaurant)
+        db.session.commit()
+        return make_response({''}, 204)
+
+@app.route('/pizzas', methods=['GET'])
+def pizzas():
+    if request.method == 'GET':
+        pizza_list = [p.to_dict() for p in Pizza.query.all()] 
+        response = make_response(
+            pizza_list,
+            200
+        )
+        return response
+
+@app.route('/restaurant_pizzas', methods = ['POST'])
+def new_restaurant():
+    data = request.get_json()
+    new_restaurant_pizza = RestaurantPizza(
+        price = data['price'],
+        pizza_id = data['pizza_id'],
+        restaurant_id = data['restaurant_id']
+    )
+    db.session.add(new_restaurant_pizza)
+    db.session.commit()
+    try:
+        return make_response(new_restaurant_pizza.pizza.to_dict(), 201)
+    except ValueError:
+        return make_response({'error': ['Invalid input']}, 400) 
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
--- server/models.py
+++ server/models.py
@@ -11,3 +11,57 @@ metadata = MetaData(naming_convention={
 db = SQLAlchemy(metadata=metadata)
 
 # Add models here
+
+class Pizza(db.Model, SerializerMixin):
+    __tablename__ = 'pizzas'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    ingredients = db.Column(db.String)
+    created_at = db.Column(db.DateTime, server_default = db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate = db.func.now())
+
+    def __repr__(self):
+        return f'Name: {self.name} | Ingredients: {self.ingredients}'
+    
+    restaurant_pizzas = db.relationship('RestaurantPizza', backref='pizza')
+    restaurants = association_proxy('restaurant_pizzas', 'restaurant')
+
+    serialize_rules = ('-created_at', '-updated_at', '-restaurants', '-restaurant_pizzas')
+
+class RestaurantPizza(db.Model, SerializerMixin):
+    __tablename__ = 'restaurant_pizzas'
+
+    id = db.Column(db.Integer, primary_key=True)
+    price = db.Column(db.Integer)
+    created_at = db.Column(db.DateTime, server_default = db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate = db.func.now())
+
+    pizza_id = db.Column(db.Integer, db.ForeignKey('pizzas.id'))
+    restaurant_id = db.Column(db.Integer, db.ForeignKey('restaurants.id'))
+    
+    @validates('price')
+    def validate_price(self, key, price):
+        if price < 1 or price > 30:
+            raise ValueError({'error': 'Price must be between 1 and 30'})
+        return price
+
+    def __repr__(self):
+        return f'Price: {self.price} | pizza_id: {self.pizza_id} | restaurant_id: {self.restaurant_id}'
+    
+    serialize_rules = ('-created_at', '-updated_at', '-pizza_id', '-restaurant_id')
+    
+class Restaurant(db.Model, SerializerMixin):
+    __tablename__ = 'restaurants'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    address = db.Column(db.String)
+
+    def __repr__(self):
+        return f'Name: {self.name} | Address: {self.address}'
+    
+    restaurant_pizzas = db.relationship('RestaurantPizza', backref='restaurant')
+    pizzas = association_proxy('restaurant_pizzas', 'pizza')
+
+    serialize_rules = ('-restaurant_pizzas', '-pizzas')
